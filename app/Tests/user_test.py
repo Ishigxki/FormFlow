5353960fe0a10c757afbdf33f330f8c0ad01a454
unknown <marseckogod@gmail.com>
2026-07-20 13:39:11 +0200

    Adding more comprehensive tests

--- app/Tests/user_test.py
+++ app/Tests/user_test.py
@@ -14,16 +14,27 @@ def test_home():
     }
 
 def test_get_user():
+
+
     response = client.get("/users")
 
     assert response.status_code == 200
 
-    assert response.json() == {
+    assert isinstance(response.json(), list)
 
-    }
+def test_get_user_by_id():
 
-def test_get_user_by_ID():
-    response =client.get("/user/{}")
+    response = client.get("/users/2")
 
     assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["id"] == 2
+
+def test_user_not_found():
+
+    response = client.get("/users/999")
+
+    assert response.status_code == 404
     
