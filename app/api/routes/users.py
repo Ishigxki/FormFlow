0660b81cca091d7ec7fd1d6d358d58108356c83e
unknown <marseckogod@gmail.com>
2026-07-21 14:49:07 +0200

    feat(auth): add bcrypt password hashing and user validation

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,15 +1,16 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException,status
 from app.schemas.user import UserCreate
 from app.api.routes.dependencies import get_db
 from app.models.user import User
+from app.security.hashing import hash_password
 from sqlalchemy.orm import Session
 
 
 router = APIRouter()
 
 
-@router.post("/users")
-def create_users(user: UserCreate, db: Session = Depends(get_db)):
+@router.post("/users", status_code=status.HTTP_201_CREATED)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
        
 
     existing_user = db.query(User).filter(User.email == user.email).first()
@@ -20,8 +21,11 @@ def create_users(user: UserCreate, db: Session = Depends(get_db)):
 
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
+    
+    
 
-    new_user = User(email=user.email,username=user.username,password_hash=user.password)
+    new_user = User(email=user.email,username=user.username,password_hash=hash_password(user.password))
+    
 
     
     db.add(new_user)
@@ -55,9 +59,19 @@ def update_user(user_id: int,user:UserCreate, db: Session = Depends(get_db)):
     if user_to_update is None:
         raise HTTPException(status_code=404, detail="User not found")
     
+    existing_email = db.query(User).filter(User.email == user.email,User.id != user_id).first()
+
+    if existing_email:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    existing_username = db.query(User).filter(User.username == user.username,User.id != user_id).first()
+
+    if existing_username:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    
     user_to_update.username = user.username
     user_to_update.email =user.email
-    user_to_update.password_hash = user.password
+    user_to_update.password_hash = hash_password(user.password)
 
 
     db.commit()
@@ -73,7 +87,7 @@ def update_user(user_id: int,user:UserCreate, db: Session = Depends(get_db)):
 def delete_user(user_id: int,db: Session = Depends(get_db)):
     user_to_delete = db.query(User).filter(User.id == user_id).first()
     if user_to_delete is None:
-        raise HTTPException(status_code=404, detail="user not found")
+        raise HTTPException(status_code=404, detail="User not found")
     
     db.delete(user_to_delete)
     db.commit()
